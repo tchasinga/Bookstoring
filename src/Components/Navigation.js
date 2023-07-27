add9c3baa5e7d0abfe1ee7bd5a61f1b4e2fcce66
Balolebwami <tchasingajacques@gmail.com>
2023-07-27 20:26:21 +0200

    new update

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -5,7 +5,7 @@ import styles from 'styles/Navbar.module.css';
 const Navbar = () => (
   <div className={styles.container}>
     <div className={styles.display}>
-      <h1 className={styles.color}>Bookstore CMS</h1>
+      <h1 className={styles.color}>Bookstore manager</h1>
       <ul>
         <li>
           <NavLink to="/" className={styles.bookLink}>
